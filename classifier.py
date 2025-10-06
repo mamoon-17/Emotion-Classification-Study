9789836ded6dfd67ba03a175e116ef1258ab7989
mamoon-17 <mamoonmasood17@gmail.com>
2025-10-07 03:36:18 +0500

    added dataset

--- classifier.py
+++ classifier.py
@@ -1,6 +1,12 @@
 from datasets import load_dataset
+import pandas as pd
 
 dataset = load_dataset("Annanay/aml_song_lyrics_balanced")
 
+#print(dataset)
+data = dataset['train']
 
-print(dataset)
+df = pd.DataFrame(data)
+
+df = df[['lyrics', 'mood', 'mood_cats']]
+print(df.head())
